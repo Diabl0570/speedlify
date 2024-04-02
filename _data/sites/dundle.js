4ca3137223e194fd935068ad4e03d16b32658bda
Kasper <Diabl0570@users.noreply.github.com>
2024-04-02 15:16:19 +0200

    dont run for 404

--- _data/sites/dundle.js
+++ _data/sites/dundle.js
@@ -10,14 +10,10 @@ module.exports = {
 		"https://next.dundle.dev/au/category/paymentcards/",
 		"https://next.dundle.dev/au/amazon/",
 		"https://next.dundle.com/au/amazon/",
-		"https://next.dundle.dev/au/",
-		"https://next.dundle.com/au/",
 
 		"https://next.dundle.com/us/category/paymentcards/",
 		"https://next.dundle.dev/us/category/paymentcards/",
 		"https://next.dundle.dev/us/amazon/",
 		"https://next.dundle.com/us/amazon/",
-		"https://next.dundle.dev/us/",
-		"https://next.dundle.com/us/",
 	]
 };

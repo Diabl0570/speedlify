d4b6c46394529dc280ec7db3b7e43ce13a9d9bdf
Kasper <Diabl0570@users.noreply.github.com>
2024-04-02 15:07:11 +0200

    dundle tests

--- _data/sites/dundle.js
+++ _data/sites/dundle.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: "Sample", // optional, falls back to object key
 	description: "The default sites that get tested",
 	options: {
-		runs: 1,
+		runs: 3,
 		frequency: 1, // (in minutes)
 	},
 	urls: [
@@ -10,5 +10,14 @@ module.exports = {
 		"https://next.dundle.dev/au/category/paymentcards/",
 		"https://next.dundle.dev/au/amazon/",
 		"https://next.dundle.com/au/amazon/",
+		"https://next.dundle.dev/au/",
+		"https://next.dundle.com/au/",
+
+		"https://next.dundle.com/us/category/paymentcards/",
+		"https://next.dundle.dev/us/category/paymentcards/",
+		"https://next.dundle.dev/us/amazon/",
+		"https://next.dundle.com/us/amazon/",
+		"https://next.dundle.dev/us/",
+		"https://next.dundle.com/us/",
 	]
 };
